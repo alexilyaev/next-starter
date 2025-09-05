e81c7c495547d4371c858163377c8328c4235f3e
Alex Ilyaev <alexilyaev@gmail.com>
2025-09-05 09:14:52 +0300

    Add eslint-config-flat-gitignore and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import gitignore from 'eslint-config-flat-gitignore';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  gitignore(),
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ];
 
